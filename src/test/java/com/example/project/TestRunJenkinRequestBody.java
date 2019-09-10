5bf2ef86d0c65fced42e4cce4dfd6b647cb75fc9
Babrah <ybabrah@deloitte.com>
2019-09-10 18:58:02 +0530

    Function to Remove Quotes from Job Name

--- src/test/java/com/example/project/TestRunJenkinRequestBody.java
+++ src/test/java/com/example/project/TestRunJenkinRequestBody.java
@@ -8,9 +8,19 @@ public class TestRunJenkinRequestBody {
 	
 	public TestRunJenkinRequestBody(String jenkin_job_name, String jenkin_build_id, String status, String test_method) {
 		super();
-		this.jenkin_job_name = jenkin_job_name;
+		this.jenkin_job_name = removeQuotes(jenkin_job_name);
 		this.jenkin_build_id = jenkin_build_id;
 		this.status = status;
 		this.test_method = test_method;
+	}
+	
+	private String removeQuotes(String name) {
+		int len = name.length();
+		
+		if(name.charAt(0) == '"' && name.charAt(len-1) == '"') {
+			name = name.substring(1, len - 1);
+		}
+		
+		return name;
 	}
 }
